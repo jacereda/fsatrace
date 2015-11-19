967dfc192cb5e44c6de1a6ec4da95e756163ba7f
Jorge Acereda <jacereda@gmail.com>
2015-11-19 18:48:29 +0100

    Trying to fix Linux build

--- unix.mk
+++ unix.mk
@@ -1,9 +1,14 @@
+SOSRCS=unix/fsatraceso.c
+
 lib: fsatrace.so
 
-fsatrace.so: unix/fsatraceso.o
-	$(CC) -shared $(LDFLAGS) unix/fsatraceso.o -o fsatrace.so
+%.os: %.c
+	$(CC) -c -fPIC $(CPPFLAGS) $(CFLAGS) $< -o $@
+
+fsatrace.so: $(patsubst %.c,%.os,$(SOSRCS))
+	$(CC) -shared $(CPPFLAGS) $(CFLAGS) -o fsatrace.so unix/fsatraceso.c $(LDFLAGS) 
 
 cleanlib:
-	rm -f fsatrace.so unix/fsatraceso.d unix/fsatraceso.o
+	rm -f fsatrace.so $(patsubst %.c,%.d,$(SOSRCS)) $(patsubst %.c,%.os,$(SOSRCS))
 
--include $(patsubst %.c,%.d,$(SRCS))
+-include $(patsubst %.c,%.d,$(SOSRCS))
