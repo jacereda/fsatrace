51d4bae06d762f0c17d132993db652417d0792f8
Jorge Acereda <jacereda@gmail.com>
2015-11-27 23:20:06 +0100

    Debugging osx lockups on travis

--- unix/fsatraceso.c
+++ unix/fsatraceso.c
@@ -250,6 +250,31 @@ unlinkat(int fd, const char *p, int f)
 	return r;
 }
 
+int
+futimes(int fd, const struct timeval t[2])
+{
+	int r;
+	static int (*ofutimes)(int, const struct timeval[2]);
+	R(futimes);
+	r = ofutimes(fd, t);
+	if (!r)
+		fdemit('t', fd);
+	return r;
+}
+
+int
+utimes(const char * p, const struct timeval t[2])
+{
+	int r;
+	static int (*outimes)(const char *, const struct timeval[2]);
+	R(utimes);
+	r = outimes(p, t);
+	if (!r)
+		emit('t', p);
+	return r;
+}
+
+
 #define SUF "$INODE64"
 
 
